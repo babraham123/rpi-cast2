4aac48c1dd7f7b92a4206e50bcc6db6c9ba3fc24
Bereket Abraham <babraham42@gmail.com>
2015-12-30 02:36:16 -0500

    Create headless_chrome.py

---
+++ headless_chrome.py
@@ -0,0 +1,24 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+
+url = 'http://www.filmon.com/tv/channel/export?channel_id=1793&affid=55882Lf'
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+# enable browser logging
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = { 'browser':'ALL' }
+browser = webdriver.Chrome(desired_capabilities=d)
+print browser.title
+
+# load site
+browser.get(url)
+time.sleep(90)
+# print messages
+for entry in browser.get_log('browser'):
+    print entry
+
+browser.quit()
+display.stop()
